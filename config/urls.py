9ceddfeb6662d8c69615a85dab882489cb8bf159
Elias <ebmm01@gmail.com>
2019-01-04 16:31:48 -0200

    Moving configuration into its own folder
    Adding __str__ test to make coverage 100%

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
-from .users.views import UserViewSet, UserCreateViewSet
+from API.users.views import UserViewSet, UserCreateViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
