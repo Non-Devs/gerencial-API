42d74a2f3267295dcc8e9ac8800448a1fed4bfd4
Elias <ebmm01@gmail.com>
2019-01-05 19:48:33 -0200

    Changing Studends Views
    Adding new students routes

--- API/studies/views.py
+++ API/studies/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from . import models
-from . import serializers
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import AllowAny
+from .models import Students
+from .serializers import StudentsSerializer
+from API.users.permissions import IsUserOrReadOnly
 
 
-class StudentsViewset(viewsets.ModelViewSet):
-    queryset = models.Students.objects.all()
-    serializer_class = serializers.StudentsSerializer
+class StudentsViewSet(mixins.RetrieveModelMixin,
+                      mixins.UpdateModelMixin,
+                      viewsets.GenericViewSet):
+    """
+    Updates and retrieves user accounts
+    """
+    queryset = Students.objects.all()
+    serializer_class = StudentsSerializer
+    permission_classes = (IsUserOrReadOnly,)
+
+
+class StudentsCreateViewSet(mixins.CreateModelMixin,
+                            viewsets.GenericViewSet):
+    """
+    Creates user accounts
+    """
+    queryset = Students.objects.all()
+    serializer_class = StudentsSerializer
+    permission_classes = (AllowAny,)
 
     def perform_create(self, serializer):
         serializer.save(teacher=self.request.user)
